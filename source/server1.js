6c0b4388dd5c2eac4833dad1b277939c60f02f54
jayavarapulikhitha <jayavarapulikhitha059@gmail.com>
2024-08-11 18:37:31 +0530

    hi

--- source/server1.js
+++ source/server1.js
@@ -24,8 +24,80 @@ app.post('/insertmany', async(req, res) => {
     })
     .catch((e)=>console.log(e))
 })
+
+
+app.post('/signin', async(req, res) => {
+    await db.collection("login").findOne({Email:req.body.Email})
+    .then((result)=>{
+        console.log(result)
+        if(result?.Password===req.body.Password)
+        {
+        res.json({message:"login success",values:result})
+        }
+        else{
+            res.json({error:"user not found"})
+        }
+    
+    })
+    .catch((e)=>console.log(e))
+})
+
+
+app.post('/signup', async(req, res) => {
+    
+    await db.collection("login").insertOne({Name:req.body.Name,Email: req.body.Email,Password : req.body.Password})
+    .then((result)=>{
+        res.json(result)
+        console.log(result);
+    })
+    .catch((e)=>console.log(e))
+})
+app.post('/signup', async(req, res) => {
+    await db.collection("login").find({Email:req.body.Email})
+    .then((result)=>{
+        console.log(result)
+        if(result?.Email===req.body.Email)
+        {
+        res.json({error:"user already have an account",values:result})
+        }
+        else{
+            res.json({message:"login successfull!"})
+        }
+    
+    })
+    .catch((e)=>console.log(e))
+})
+
+app.post('/forgetpassword', async(req, res) => {
+    await db.collection("login").findOne({Email:req.body.email})
+    .then((result)=>{
+        
+        res.json({message:"change password",values:result})
+        
+    })
+    .catch((e)=>console.log(e))
+})
+app.post('/forgetpassword', async(req, res) => {
+    await db.collection("login").updateOne({Password:req.body.password})
+    .then((result)=>{
+        
+        res.json({message:"password changed",values:result})
+        
+    })
+    .catch((e)=>console.log(e))
+})
+
+
+app.post('/students', async(req, res) => {
+    
+    await db.collection("login").find().toArray()
+     .then((result)=>{
+         res.send(result)
+      }).catch((e)=>console.log(e))})
+
+
 connectToDB(() => {
-    app.listen(8000, () => {
-        console.log("server running at 8000");
+    app.listen(9000, () => {
+        console.log("server running at 9000");
     })
 })
